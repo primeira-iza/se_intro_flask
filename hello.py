81326a81107a67e95791b280d4aa056a8d48fbac
Student <student@TesterVM-59.edu-wsb.local>
2021-01-30 11:54:03 +0100

    add calculator

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import request
 app = Flask(__name__)
 
 moje_imie = input("What's your name? ")
@@ -20,3 +21,19 @@ def wiadomosc():
 @app.route('/hobby')
 def wiadomosc():
     return moje_hobby
+
+# http://127.0.0.1:5000/oblicz?l1=10&l2=20&op=plus
+@app.route('/oblicz')
+def oblicz():
+    l1_arg = request.args.get('l1')
+    if l1_args is None:
+        return 'brak argumentu l1'
+    l1 = int(l1_arg)
+
+    l2_arg = request.args.get('l2')
+    l2 = int(l2_arg)
+    op = request.args.get('op')
+    if op == 'plus':
+        s = l1 + l2
+        return f'{s}'
+    return ""
